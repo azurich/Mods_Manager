8194e8ce1c7ef6d15a8d187d260f05e7fc0e8c55
Quentin <112642301+QuentinBAUDRY@users.noreply.github.com>
2025-04-30 09:28:57 +0200

    Update build_and_push.bat

--- Mods_Manager/build_and_push.bat
+++ Mods_Manager/build_and_push.bat
@@ -2,29 +2,29 @@
 setlocal
 
 :: === CONFIG À MODIFIER ICI ===
-set "GIT_REPO_PATH=C:\Users\Quentin\Documents\GitHub\updater"
-set "VERSION=1.4"
+set "GIT_REPO_PATH=C:\Users\qbaudry\Documents\Mods_Manager\Mods_Manager"
+set "VERSION=1.7"
 
 :: === Nettoyage ===
 echo.
-echo 🔄 Nettoyage des anciens fichiers...
+echo Nettoyage des anciens fichiers...
 rmdir /s /q build
 rmdir /s /q dist
 del /q *.spec
 
 :: === Compilation ===
 echo.
-echo 🛠 Compilation de main.py...
+echo Compilation de main.py...
 pyinstaller --onefile --noconsole --icon=assets/app.ico main.py
 
 :: === Renommage ===
 echo.
-echo ✏️  Renommage de l'exécutable...
+echo Renommage de l executable...
 rename dist\main.exe main_update.exe
 
 :: === Copie dans le repo GitHub ===
 echo.
-echo 📁 Copie dans le dépôt GitHub local...
+echo Copie dans le depot GitHub local...
 copy /Y dist\main_update.exe "%GIT_REPO_PATH%\main_update.exe"
 
 :: === Mise à jour du version.txt ===
@@ -34,9 +34,9 @@ echo %VERSION% > "%GIT_REPO_PATH%\version.txt"
 echo.
 cd /d "%GIT_REPO_PATH%"
 git add main_update.exe version.txt
-git commit -m "🔄 Mise à jour Mods Manager v%VERSION%"
+git commit -m "Mise a jour Mods Manager v%VERSION%"
 git push
 
 echo.
-echo ✅ Mise à jour poussée sur GitHub avec succès !
+echo Mise a jour poussee sur GitHub avec succes !
 pause
